4899fb88056d5e07263182741a62b58dfdd25e23
Fasih Awan <fasihawan@gmail.com>
2016-01-25 19:53:57 -0500

    Cleaned up code

--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/Ommy/assignment2/ComputeBigramRelativeFrequencyStripes.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/Ommy/assignment2/ComputeBigramRelativeFrequencyStripes.scala
@@ -37,26 +37,6 @@ object ComputeBigramRelativeFrequencyStripes extends Tokenizer {
     if (!args.imc()) {
       textFile
         .flatMap(line => {
-//          var stripes:Map[String, Map[String, Float]] = Map()
-//          if (tokens.length > 2) {
-//            for (x <- 1 to tokens.length-1) {
-//              val prev = tokens(x-1)
-//              val curr = tokens(x)
-//              if (stripes.contains(prev)) {
-//                var stripe:Map[String, Float] = stripes(prev)
-//                if (stripe.contains(curr)) {
-//                  val count:Float = stripe(curr)
-//                  stripe = stripe + (curr -> (count + 1.0f))
-//                  stripes = stripes + (curr -> stripe)
-//                } else {
-//                  stripe = stripe + (curr -> 1.0f)
-//                  stripes = stripes + (curr -> stripe)
-//                }
-//              } else {
-//                val stripe:Map[String, Float] = Map(curr -> 1.0f)
-//                stripes = stripes + (prev -> stripe)
-//              }
-//            }
           val tokens:List[String] = tokenize(line)
           var stripes:Map[String, Map[String, Float]] = Map()
           tokens.sliding(2).take(tokens.length-1).foreach((x) => {
@@ -66,25 +46,6 @@ object ComputeBigramRelativeFrequencyStripes extends Tokenizer {
               stripes += (x.head -> (stripes(x.head) + (x(1) -> (1.0f + stripes(x.head).getOrElse(x(1), 0.0f)))))
             }
           })
-//          if (tokens.length > 2) {
-//            for (x <- 1 to tokens.length-1) {
-//              val prev = tokens(x-1)
-//              val curr = tokens(x)
-//              if (stripes.contains(prev)) {
-//                var stripe:Map[String, Float] = stripes(prev)
-//                if (stripe.contains(curr)) {
-//                  val count:Float = stripe(curr)
-//                  stripe = stripe + (curr -> (count + 1.0f))
-//                  stripes = stripes + (curr -> stripe)
-//                } else {
-//                  stripe = stripe + (curr -> 1.0f)
-//                  stripes = stripes + (curr -> stripe)
-//                }
-//              } else {
-//                val stripe:Map[String, Float] = Map(curr -> 1.0f)
-//                stripes = stripes + (prev -> stripe)
-//              }
-//            }
           stripes
         })
         .map(m => {
@@ -92,7 +53,7 @@ object ComputeBigramRelativeFrequencyStripes extends Tokenizer {
         })
         .reduceByKey((x, y) => {
           x ++ y.map{ case (k,v) => k -> (v + x.getOrElse(k, 0.0f)) }
-        })
+        }, args.reducers())
         .repartitionAndSortWithinPartitions(pp)
         .mapPartitions(item => {
           item.map((f) => {
@@ -104,62 +65,7 @@ object ComputeBigramRelativeFrequencyStripes extends Tokenizer {
               ((f._1, i._1), i._2 / sum)
             })
           })
-//          val key:String = item._1
-//          val maps:Map[String, Float]] = item._2
-//
-//          var result:Map[String, Float] = Map()
-//
-//          maps.foreach((f) => {
-//            f.foreach((m) => {
-//              if (result.contains(m._1)) {
-//                val t:Float = result(m._1)
-//                result = result + (m._1 -> (t+m._2))
-//              } else {
-//                result = result + m
-//              }
-//            })
-//          })
-//
-//          var sum:Float = 0.0f
-//          result.foreach((f) => {
-//            sum = sum + f._2
-//          })
-//
-//          result.foreach((f) => {
-//            val t:Float = f._2
-//            result = result + (f._1 -> (t/sum))
-//          })
-//          (key, result)
         })
-//        .reduceByKey((a:Map[String, Float],b:Map[String, Float]) => {
-//          a
-//            .toSeq.++(b.toSeq)
-//            .groupBy(_._1)
-//            .mapValues(_.map(_._2).reduce(_+_))
-
-//          var sum:Float = 0.0f
-//          merged.foreach(element => {
-//            sum = sum + element._2
-//          })
-//          var result:Map[String, Float] = Map()
-//          for (x <- merged.keys) {
-//            result = result + (x -> sum)
-//          }
-//          result
-
-//        }).groupByKey()
-//        .map(w => {
-//          val merged = w._2
-//          var sum:Float = 0.0f
-//          merged.foreach(element => {
-//            sum = sum + element.
-//          })
-//          var result:Map[String, Float] = Map()
-//          for (x <- merged.keys) {
-//            result = result + (x -> (merged(x)/sum))
-//          }
-//
-//        })
         .saveAsTextFile(args.output())
     } else {
 
